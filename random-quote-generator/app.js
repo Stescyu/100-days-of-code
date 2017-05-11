9f77f5bde180940b3c5b7b0ecbe5bf2f1a1b0c64
Stescyu <stevenyusc@gmail.com>
2017-05-11 16:20:30 -0400

    added all basic files for quote generator. Major API functional

---
+++ random-quote-generator/app.js
@@ -0,0 +1,31 @@
+//all npm installs declared
+var express = require("express");
+var app = express();
+var request = require("request");
+var port = process.env.PORT || 3000;
+
+//res.render("quotegen.ejs"); => res.render("quotegen");
+app.set("view engine", "ejs");
+
+//declared static directory
+app.use(express.static(__dirname + '/public'));
+
+//use request to get the body elements and render into quotegen page
+app.get("/", function(req,res){
+    //endpoint
+    var url = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+    //request for body
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200){
+            //parses the string into object
+            var quote = JSON.parse(body);
+            var actquote = quote["0"]["content"];
+            var actauthor = quote["0"]["title"];
+            res.render("quotegen", {quote: actquote, author: actauthor});
+        }
+    })
+});
+
+//spin up server
+app.listen(port);
+console.log("Server Started at port " + port);
