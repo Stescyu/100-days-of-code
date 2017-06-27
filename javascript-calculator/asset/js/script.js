81bf30af4ec3e8ed94a79d5257bc355a45654717
Stescyu <stevenyusc@gmail.com>
2017-06-27 13:45:48 -0400

    Added Javascript Calculator, done design

---
+++ javascript-calculator/asset/js/script.js
@@ -0,0 +1,15 @@
+//Sets the value output
+
+var value = 0;
+
+$("#allClear").on("click", function(){
+   value = 0;
+    $("#bigDisplay").append(0);
+});
+
+// function inputNum(){
+//     if($("#bigDisplay").text() == 0{
+
+//     }
+// }
+
